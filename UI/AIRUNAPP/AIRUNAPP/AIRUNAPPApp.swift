700055aa73e679a7cee33240463c36d436dc8d77
Niam Bashambu <niambashambu@icloud.com>
2024-09-14 14:05:58 -0400

    adding app stuff from xcode

---
+++ UI/AIRUNAPP/AIRUNAPP/AIRUNAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIRUNAPPApp.swift
+//  AIRUNAPP
+//
+//  Created by Niam Bashambu on 9/14/24.
+//
+
+import SwiftUI
+
+@main
+struct AIRUNAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
