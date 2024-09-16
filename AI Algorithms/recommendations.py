7fef32599cb6a87509f366193123dfde1d803730
Niam Bashambu <niambashambu@icloud.com>
2024-09-16 19:41:18 -0400

    comments

--- AI Algorithms/recommendations.py
+++ AI Algorithms/recommendations.py
@@ -15,6 +15,7 @@ def calculate_pace(distance, moving_time):
     return pace / 60  # convert to minutes per km
 
 # Function to generate recommendations based on activity data
+#need to make the recconmendations change based off the runners actual performance
 def generate_recommendations(activity):
     recommendations = []
 
